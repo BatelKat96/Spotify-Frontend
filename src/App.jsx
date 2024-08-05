258f3b4987fee925aa6133b10516951c907e8a89
BatelKat96 <batelkat@gmail.com>
2024-08-05 14:10:52 +0300

    Build main routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
+import { Route, Routes } from 'react-router';
+import { AppHeader } from './cmps/AppHeader';
+import { Search } from './pages/Search';
+import { StationDetails } from './pages/StationDetails';
+import { StationIndex } from './pages/StationIndex';
+import { SideBar } from './cmps/SideBar';
+import { AppFooter } from './cmps/AppFooter';
+
 export function App() {
   return (
-    <>
-      Hello spotify
-    </>
+    <div className="main-layout">
+      <SideBar />
+
+      <main>
+        <AppHeader />
+        <Routes>
+          <Route path="" element={<StationIndex />} />
+          <Route path="/station/:stationId" element={<StationDetails />} />
+          <Route path="search" element={<Search />} />
+        </Routes>
+      </main>
+
+      <AppFooter />
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,18 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import { BrowserRouter as Router } from 'react-router-dom'
+// import { Provider } from 'react-redux'
+
 import { App } from './App.jsx'
+
 import './assets/main.scss'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  // <Provider>
+  <Router>
+    <App />
+  </Router>
+  // </Provider>
 )
