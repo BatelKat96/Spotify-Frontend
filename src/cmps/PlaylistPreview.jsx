28cdf912f31848fa95c5fe1e16b3605fdfc26ec0
BatelKat96 <batelkat@gmail.com>
2025-05-06 15:18:51 +0300

    Render home page data + UI

--- src/cmps/PlaylistPreview.jsx
+++ src/cmps/PlaylistPreview.jsx
@@ -1,8 +1,18 @@
 export function PlaylistPreview({ playlist }) {
+
+    function detectLanguage(txt) {
+        const hebrewPattern = /[\u0590-\u05FF]/;
+
+        if (hebrewPattern.test(txt)) {
+            return 'rtl';
+        }
+    };
+
     return (
-        <>
+        <div style={{ direction: detectLanguage(playlist.title) }}>
             <img src={playlist.imgUrl} />
-            <p>{playlist.title}</p>
-        </ >
+            <p className='playlist-title'>{playlist.title}</p>
+            <p className='playlist-description'>{playlist.description}</p>
+        </div >
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,50 @@
-import { useSelector } from 'react-redux'
-import { useEffect } from 'react'
-
+import { useEffect, useRef, useState } from 'react'
 import { PlaylistList } from '../cmps/PlaylistList'
-import { loadPlaylists } from '../store/actions/playlist.actions'
+
+import data from '../data/HomePagePlaylist.json'
 
 
 export function Home() {
-
-    const playlists = useSelector(storeState => storeState.playlistModule.playlists)
-
     useEffect(() => {
-        loadPlaylists()
+        console.log('data:', data)
     }, [])
 
 
-    console.log('playlists:', playlists)
+    const [sectionWidth, setSectionWidth] = useState(0)
+    const sectionRef = useRef(null)
+
+    useEffect(() => {
+        const sectionElement = sectionRef.current
+        if (!sectionElement) return
+
+        const resizeObserver = new ResizeObserver((entries) => {
+            // for (let entry of entries) {
+            //     setSectionWidth(entry.contentRect.width)
+            // }
+            const entry = entries[0]
+            setSectionWidth(entry.contentRect.width)
+        })
+
+        resizeObserver.observe(sectionElement)
+
+        return () => {
+            resizeObserver.unobserve(sectionElement);
+        }
+    }, [])
+
+
 
     return (
-        <section className='home'>
-            <PlaylistList playlists={playlists} />
+        <section className='home' ref={sectionRef}>
+            <h3>Pop</h3>
+            <PlaylistList playlists={data.pop} width={sectionWidth} />
+            <h3>Hip-hop</h3>
+            <PlaylistList playlists={data.hiphop} width={sectionWidth} />
+            <h3>Latin</h3>
+            <PlaylistList playlists={data.latin} width={sectionWidth} />
+            <h3>Decades</h3>
+            <PlaylistList playlists={data.decades} width={sectionWidth} />
+
         </section >
     )
 }
